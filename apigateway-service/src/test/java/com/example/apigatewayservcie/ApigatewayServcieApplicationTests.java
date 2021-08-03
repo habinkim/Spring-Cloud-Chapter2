e25f160a915fd10f397609a114350a5f09f7f3a3
habin <danny9643@naver.com>
2021-08-03 10:04:23 +0900

    First Commit

---
+++ apigateway-service/src/test/java/com/example/apigatewayservcie/ApigatewayServcieApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.apigatewayservcie;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApigatewayServcieApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
